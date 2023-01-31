cb82121d6f53b74f5322e6716810d83d3ca50bfb
Timothy Mahoney <timothy.mahoney1@ingka.ikea.com>
2023-01-31 11:57:56 +0100

    feat: jaeger:current w/ otelt

--- src/extension.ts
+++ src/extension.ts
@@ -17,7 +17,7 @@ import * as cp from 'child_process';
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
 		vscode.commands.registerCommand('jaeger.start', () => {
-			cp.exec('docker run -d --name jaeger -e COLLECTOR_ZIPKIN_HTTP_PORT=9411 -p 5775:5775/udp -p 6831:6831/udp -p 6832:6832/udp -p 5778:5778 -p 16686:16686 -p 14268:14268 -p 9411:9411 -p 4317:4317 jaegertracing/all-in-one:1.41', (err, stdout, stderr) => {
+			cp.exec('docker run -d --name jaeger -e COLLECTOR_ZIPKIN_HTTP_PORT=9411 -e COLLECTOR_OTLP_ENABLED=true -p 6831:6831/udp -p 6832:6832/udp -p 5778:5778 -p 16685:16685 -p 16686:16686 -p 4317:4317 -p 4318:4318 -p 14250:14250 -p 14268:14268 -p 14269:14269 -p 9411:9411 jaegertracing/all-in-one:latest', (err, stdout, stderr) => {
 				if (err) {
 					vscode.window.showErrorMessage(err.message)
 				} else {
